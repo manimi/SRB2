5b0260304d8be920478340e70f49164929da9edc
Manimi Fire <manimifire@gmail.com>
2020-05-08 16:59:29 +0300

    trying to change something in the f_finale.c

--- src/f_finale.c
+++ src/f_finale.c
@@ -4083,7 +4083,6 @@ void F_CutsceneTicker(void)
 
 	// advance animation
 	finalecount++;
-	cutscene_boostspeed = 0;
 
 	dofadenow = false;
 
@@ -4098,6 +4097,10 @@ void F_CutsceneTicker(void)
 			cutscene_boostspeed = 1;
 			if (timetonext)
 				timetonext = 2;
+		}
+		else
+		{
+			cutscene_boostspeed = 0;
 		}
 	}
 
